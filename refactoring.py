4f26b403148f7b1d1b64e7837144a2a2abc53411
mmubashirdev <muhammadmubashirramzan886@gmail.com.com>
2025-11-12 08:53:20 +0500

    modified refactoring.py

--- refactoring.py
+++ refactoring.py
@@ -2,4 +2,4 @@ student = ['Ahmed', 'Araiz','Jawad','Hanzla']
 for i in student:
   print(f"Welcome {i} to LGU")
   
-print("All students have been welcomed.")
+print("All students have been welcomed." )
